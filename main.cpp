3645eb02608fbc8bf21dbeb4d83c1e88f13968c5
Oscar <oticonaf@unjbg.edu.pe>
2024-11-21 22:15:22 -0500

    Agrega proyecto

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int op;
+	
+	do {
+		cout<<"---------MENU PRINCIPAL---------"<<endl
+			<<"1. Calcular Trabajo de una fuerza constante"<<endl
+			<<"2. Calcular Trabajo de una fuerza variable"<<endl
+			<<"3. Calcular Trabajo consumido por un resorte"<<endl
+			<<"4. Calcular Potencia"<<endl
+			<<"0. Salir del programa"<<endl
+			<<"Ingrese una opcion: "; cin>>op;
+		switch (op) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+		}
+			
+		
+	} while (op!=0);
+	return 0;
+}
