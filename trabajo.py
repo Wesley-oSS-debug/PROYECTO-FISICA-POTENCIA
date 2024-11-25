8e9ae183377b0e85fd947bf183918564963a0d80
Oscar <oticonaf@unjbg.edu.pe>
2024-11-24 19:56:42 -0500

    Actualiza codigo, se hace uso de frames para organizar mejor los widgets

--- trabajo.py
+++ trabajo.py
@@ -10,8 +10,11 @@ raiz.geometry("800x800+500+100")
 raiz.resizable("False","False")
 raiz.config(bg="#ffecbd")
 mainImage=PhotoImage(file="trabajo.png")
-imgLbl=Label(raiz,image=mainImage)
-imgLbl.grid(row=0,column=0,padx=150,pady=10)
+#frame de la imagen
+frameImg=Frame(raiz,bg="#ffecbd")
+frameImg.grid(row=0,column=0,pady=30)
+imgLbl=Label(frameImg,image=mainImage)
+imgLbl.grid(row=0,column=0,padx=140)
 
 def salir():
     valor=messagebox.askokcancel("Salir","Estas seguro que deseas salir?")
@@ -52,86 +55,95 @@ def trabajoC():
         vDat=Tk()
         vDat.title("Datos")
         vDat.geometry("550x400+100+300")
-        
+        vDat.resizable(0,0)
+        frameLista=Frame(vDat)
+        frameLista.grid(padx=2)
         #Labels
-        fLbl=Label(vDat, text="Fuerza", bg="yellow", width=14, height=1)
+        fLbl=Label(frameLista, text="Fuerza", bg="yellow", width=14, height=1)
         fLbl.grid(row=0,column=0, padx=10, pady=5)
         
-        aLbl=Label(vDat, text="Angulo",bg="yellow", width=14, height=1)
+        aLbl=Label(frameLista, text="Angulo",bg="yellow", width=14, height=1)
         aLbl.grid(row=0,column=1, padx=10, pady=5)
         
-        dLbl=Label(vDat, text="Distancia",bg="yellow", width=14, height=1)
+        dLbl=Label(frameLista, text="Distancia",bg="yellow", width=14, height=1)
         dLbl.grid(row=0,column=2, padx=10,pady=5)
         
-        tLbl=Label(vDat,text="Trabajo",bg="yellow", width=14, height=1)
+        tLbl=Label(frameLista,text="Trabajo",bg="yellow", width=14, height=1)
         tLbl.grid(row=0,column=3, padx=10, pady=5)
         
         #Entrys
         
         for i in range(len(datos[0])): #solo es necesario la longitud de un subarreglo
             
-            fIn=Entry(vDat, justify="center")
+            fIn=Entry(frameLista, justify="center")
             fIn.grid(row=i+1,column=0,padx=10)
             fIn.insert(0,datos[0][i])
             fIn.config(state="readonly")
 
-            aIn=Entry(vDat,justify="center")
+            aIn=Entry(frameLista,justify="center")
             aIn.grid(row=i+1,column=1)
             aIn.insert(0,datos[1][i])
             aIn.config(state="readonly")
             
-            dIn=Entry(vDat,justify="center")
+            dIn=Entry(frameLista,justify="center")
             dIn.grid(row=i+1,column=2, padx=10)
             dIn.insert(0,datos[2][i])
             dIn.config(state="readonly")
             
-            tIn=Entry(vDat,justify="center")
+            tIn=Entry(frameLista,justify="center")
             tIn.grid(row=i+1,column=3)
             tIn.insert(0,datos[3][i])
             tIn.config(state="readonly")
 
     #LABEL,ENTRY Y BOTONES
-    fuerzaLbl=Label(vTF, text="Fuerza:")
-    fuerzaLbl.grid(row=0,column=0, sticky="e", padx=10, pady=5)
     
-    fuerzaIn=Entry(vTF, justify="center")
+    frameEnt=Frame(vTF)
+    frameEnt.grid(padx=10)
+    
+    fuerzaLbl=Label(frameEnt, text="Fuerza:")
+    fuerzaLbl.grid(row=0,column=0, sticky="nsew", padx=10, pady=5)
+    
+    fuerzaIn=Entry(frameEnt, justify="center")
     fuerzaIn.grid(row=0,column=1)
     
-    anguloLbl=Label(vTF, text="Angulo:")
-    anguloLbl.grid(row=1,column=0, sticky="e", padx=10, pady=5)
+    anguloLbl=Label(frameEnt, text="Angulo:")
+    anguloLbl.grid(row=1,column=0, sticky="nsew", padx=10, pady=5)
     
-    anguloIn=Entry(vTF,justify="center")
+    anguloIn=Entry(frameEnt,justify="center")
     anguloIn.grid(row=1,column=1)
     
-    distanciaLbl=Label(vTF, text="Distancia:")
-    distanciaLbl.grid(row=2,column=0, sticky="e", padx=10,pady=5)
+    distanciaLbl=Label(frameEnt, text="Distancia:")
+    distanciaLbl.grid(row=2,column=0, sticky="nsew", padx=10,pady=5)
     
-    distanciaIn=Entry(vTF,justify="center")
+    distanciaIn=Entry(frameEnt,justify="center")
     distanciaIn.grid(row=2,column=1)
     
-    btn=Button(vTF, text="Calcular", command=calculo)
+    btn=Button(frameEnt, text="Calcular", command=calculo)
     btn.grid(row=1,column=2, padx=10)
     
-    resultadoLbl=Label(vTF,text="Resultado:")
-    resultadoLbl.grid(row=3,column=0, padx=10, pady=5)
+    resultadoLbl=Label(frameEnt,text="Resultado:")
+    resultadoLbl.grid(row=3,column=0,sticky="nsew", padx=10, pady=5)
     
-    resultadoIn=Entry(vTF, readonlybackground="yellow",state="readonly",justify="center")
+    resultadoIn=Entry(frameEnt, readonlybackground="yellow",state="readonly",justify="center")
     resultadoIn.grid(row=3,column=1)
     
-    mostrarBtn=Button(vTF, text="Mostrar Datos", command=mostrarDatos)
+    mostrarBtn=Button(frameEnt, text="Mostrar Datos", command=mostrarDatos)
     mostrarBtn.grid(row=4,column=1, pady=30)
 
-#BOTONES
-btn1=Button(raiz, text="Teoria de TRABAJO")
+#BOTONES MENU PRINCIPAL
+#frame de botones
+frameBtn=Frame(raiz, bg="#ffecbd")
+frameBtn.grid()
+btn1=Button(frameBtn, text="Teoria de TRABAJO")
 btn1.grid(row=1,column=0,pady=10)
 
-btn2=Button(raiz, text="Calcular Trabajo de una fuerza constante", command=trabajoC)
+btn2=Button(frameBtn, text="Calcular Trabajo de una fuerza constante", command=trabajoC)
 btn2.grid(row=2,column=0,pady=10)
 
-btn3=Button(raiz, text="Calcular Trabajo de una fuerza variable")
+btn3=Button(frameBtn, text="Calcular Trabajo de una fuerza variable")
 btn3.grid(row=3,column=0,pady=10)
 
-btn0=Button(raiz, text="Salir del programa", fg="red", command=salir)
+btn0=Button(frameBtn, text="Salir del programa", fg="red", command=salir)
 btn0.grid(row=4,column=0,pady=10)
 
 raiz.mainloop()
